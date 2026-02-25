eb1a8fbede1138fe7e650f101cbe6a856c6f711d
AlexsanderGS <alexsanderga@gmail.com>
2026-02-25 17:02:31 -0300

    Praticando JS: Laços - Contagem de números.

---
+++ praticando-lacos/contagem-de-numeros.js
@@ -0,0 +1,11 @@
+/* Você está criando um aplicativo para exibir a sequência de números até um valor que o usuário forneceu. O objetivo é mostrar todos os números a partir de 1 até o valor informado pelo usuário, de forma ordenada e crescente.
+
+Como você pode gerar essa contagem e garantir que cada número seja mostrado até atingir o valor final fornecido? */
+
+const numeroFinal = 5;
+
+resultado = 0;
+
+for (let i = 1; i <= numeroFinal; i++) {
+  console.log("Números inseridos: ", i);
+}
