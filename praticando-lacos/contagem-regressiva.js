83f79848023771a2c125b68ab45b67544928a469
AlexsanderGS <alexsanderga@gmail.com>
2026-02-25 17:07:33 -0300

    Praticando JS: Laços - Contagem regressiva.

---
+++ praticando-lacos/contagem-regressiva.js
@@ -0,0 +1,11 @@
+/* Você é uma pessoa desenvolvedora de interface em uma startup de tecnologia aeroespacial e precisa criar uma contagem regressiva automática para o painel de lançamento. A contagem deve começar de 10 até 0, exibindo “Lançar!” ao final.
+
+Crie um programa que conte de 10 até 0 e exiba a mensagem “Lançar!” ao final. */
+
+const contagem = 0;
+
+for (let i = 10; i >= contagem; i--) {
+  console.log(i);
+}
+
+console.log("Lançar!");
